3123072700e59a73aa6695cb4989e868365c52ba
thatsLegit <iliastepanov1996@gmail.com>
2022-10-31 16:44:47 +0100

    sign-in/sign-up easthetics

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id                 :integer          not null, primary key
+#  content            :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
 class Comment < ApplicationRecord
   validates_length_of :content, minimum: 3
   validates_length_of :content, maximum: 255
